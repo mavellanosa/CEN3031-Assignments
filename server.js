49173cb9086b6c39f21b305729ef08c749fe90aa
mavellanosa <mavellanosa@ufl.edu>
2017-08-30 21:05:16 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,51 @@
+var http = require('http'), 
+    fs = require('fs'), 
+    url = require('url'),
+    port = 8080;
+
+/* Global variables */
+var listingData, server;
+
+var requestHandler = function(request, response) {
+  var parsedUrl = url.parse(request.url);
+
+  console.log('I listen to you in here too');
+
+  if(parsedUrl.pathname == '/listings'){
+
+    //send listingData in the JSON format
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(listingData);  
+    response.end();
+  }
+  else{
+    
+    //Send a 404 error
+    response.writeHead(404, 'Bad gateway error');
+    response.write('Bad gateway error');
+    response.end();
+  }
+  /*
+    Your request handler should send listingData in the JSON format if a GET request 
+    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+
+    HINT: explore the request object and its properties 
+    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+   */
+};
+
+fs.readFile('listings.json', 'utf8', function(err, data) {
+
+  /*
+    This callback function should save the data in the listingData variable, 
+    then start the server. 
+   */
+   listingData = data;
+  
+   server = http.createServer(requestHandler); //This starts the server
+
+   server.listen(port, function(){
+    console.log('server is listening');
+   });
+
+});
